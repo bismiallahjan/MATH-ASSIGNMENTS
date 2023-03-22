fce87c2c81f2f2d444636f215fdcf231c5842c25
bismiallah wafadar <122517060+bismiallahjan@users.noreply.github.com>
2023-03-22 00:29:15 -0700

    Add files via upload

--- khan.py
+++ khan.py
@@ -1,22 +1,5 @@
-
-# this is multipication of two vector 
-# bismiallah wafadar
-# Teacher : Naved Himatmal
-# import matplotlib . pyplot as plt
-# Python program to demonstrate
-# circle drawing
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-import turtle as t
-	# t.pencolor('red')
-# Initializing the turtle
-t = t.Turtle()
-t.pencolor('red')
-t.pensize(4)
-
-
-# for i in range:(190)
-    
-r = 50
-t.circle(r)
-
+v1=[2,4];
